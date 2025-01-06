1a991f8c35bdc91cc7db1397e320a9fc7bd63ae5
Denisa-Maria Ciuculescu <denisa.ciuculescu@student.upt.ro>
2025-01-06 16:46:29 +0200

    dssd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,2 @@
 ﻿Console.WriteLine("ziua buna!");
+Console.WriteLine("");
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7d7cc829aeb38e165992203ea7f771497f0676d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cca59ecbafce517eefbe844d58ae3f46b7060d05")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
